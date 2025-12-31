71858860ebc0b54c8e06527b986a7d22cf38e27f
Subhamm_ <lastw5232@gmail.com>
2025-12-31 19:10:36 +0530

    some tests

--- app/models/user.py
+++ app/models/user.py
@@ -8,11 +8,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(300), nullable=False)
     hostel = db.Column(db.String(100), nullable=False)
     room_no = db.Column(db.String(20), nullable=False)
-    role = db.Column(
-        db.String(20),
-        nullable=False,
-        default="student"  
-    )
+    role = db.Column(db.String(20),nullable=False,default="student")
+    
     diaries = db.relationship('HostelDiary', backref='user', lazy=True)
     diary_comments = db.relationship('DiaryComment', backref='user', lazy=True)
     diary_likes = db.relationship('DiaryLike', backref='user', lazy=True)
